035332d34a3f8ddf912c23a58c0909a99c654bd3
RonSardD <165393807+RonSardD@users.noreply.github.com>
2024-07-15 10:02:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Fonction pour extraire les données des tables HTML
     function extractDataFromTable(table) {
         const rows = Array.from(table.querySelectorAll('tr'));
-        const categories = [];
+        const categories = ['th'];
         const seriesData = [];
 
         rows.slice(1).forEach(row => { // Slice to skip the header row
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
             categories: data1.categories,
             series: [
                 {
-                    name: 'Data1',
+                    name: 'Table1',
                     data: data1.seriesData
                 }
             ]
@@ -81,7 +81,7 @@ document.addEventListener('DOMContentLoaded', () => {
         options: {
             chart: {
                 width: chartContainer1.clientWidth,
-                height: 400
+                height: 500
             }
         }
     });
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
             categories: data2.categories,
             series: [
                 {
-                    name: 'Data2',
+                    name: 'Table2',
                     data: data2.seriesData
                 }
             ]
@@ -105,34 +105,3 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
-
-$(document).ready(function(){   
-	$('a.statexp-vislet').on('click', function (evt) {
-		
-		$this = $(this);
-		
-		var src = $this.attr('href');
-		var width = $this.attr('data-width');
-		var height = $this.attr('data-height');
-		
-		var title = '';
-		var footpage = '';
-
-		if ($this.attr('data-title') !== undefined) {
-			title = '<h4>'+$this.attr('data-title')+'</h4>';
-		}
-
-		if ($this.attr('data-footpage') !== undefined) {
-			footpage = ''+$this.attr('data-footpage')+'</p>';
-		}
-		
-		if ($this.attr('data-video') === undefined) {
-			evt.stopPropagation();
-			evt.preventDefault();
-			newwindow = window.open('', "vislet", "resizable=yes, scrollbars=yes, titlebar=no, width="+width+", height="+height+", top=10, left=10");
-			newwindow.document.write('<div><div>'+title+'</div><iframe src="'+src+'" width="100%" height="100%"></iframe><p style="text-align: right; color: grey; padding-right: 7px; font-size: 10px; font-style: italic;">'+footpage+'</p></div>');
-			newwindow.document.close();
-		}
-	});
-});
-
